7d53d7b3cbb8aa3b01504975141afaf927311e33
Mohamed Ashraf <primary0@gmail.com>
2012-08-15 20:16:47 +0800

    Return entire response hash instead of data node on user_follows method

--- lib/instagram/client/users.rb
+++ lib/instagram/client/users.rb
@@ -63,7 +63,7 @@ module Instagram
         options = args.last.is_a?(Hash) ? args.pop : {}
         id = args.first || "self"
         response = get("users/#{id}/follows", options)
-        response["data"]
+        return response
       end
     end
 
